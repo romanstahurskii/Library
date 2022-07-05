26d91a5105b4c137ec499bdd97062528dad2cbea
romanstahurskii <romanstahurskii@datasport.com>
2022-07-05 13:29:55 +0300

    add book repo

---
+++ src/main/java/com/example/library/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.example.library.repository;
+
+import com.example.library.entity.BookEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<BookEntity, Long> {
+
+    boolean existsById(Long id);
+}
